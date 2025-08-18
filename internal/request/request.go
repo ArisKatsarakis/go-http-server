4d6c9c6842965036a34717a5bb43edb057da0394
ArisKatsarakis <ariskatsarakis@hotmail.com>
2025-08-18 08:33:05 +0300

    now we are parsing bytes till the request is finished.

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,9 +1,17 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"strings"
+)
+
+type parseState string
+
+const (
+	ParseInit parseState = "init"
+	ParseDone parseState = "done"
 )
 
 type RequestLine struct {
@@ -14,33 +22,77 @@ type RequestLine struct {
 
 type Request struct {
 	RequestLine RequestLine
+	ParseState  parseState
+}
+
+func (r *Request) parse(b []byte) (int, error) {
+	read := 0
+outer:
+	for {
+
+		switch r.ParseState {
+		case ParseInit:
+		case ParseDone:
+			break outer
+		}
+	}
+	return read, nil
+}
+
+func (r *Request) done() bool {
+	return r.ParseState == ParseDone
+}
+func newRequest() *Request {
+	return &Request{
+		ParseState: ParseInit,
+	}
 }
 
 var ERR_BAD_REQUEST = fmt.Errorf("this an error line")
-var SEPERATOR = "\r\n"
+var SEPERATOR = []byte("\r\n")
 
-func parseRequestLine(b string) (*RequestLine, string, error) {
-	idx := strings.Index(b, SEPERATOR)
+func parseRequestLine(b []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(b, SEPERATOR)
 	if idx == -1 {
-		return nil, b, nil
+		return nil, 0, nil
 	}
 
 	starLine := b[:idx]
-	restOfMsg := b[idx+len(SEPERATOR):]
-	parts := strings.Split(starLine, " ")
+	read := idx + len(SEPERATOR)
+	parts := bytes.Split(starLine, []byte(" "))
 	if len(parts) != 3 {
-		return nil, restOfMsg, ERR_BAD_REQUEST
+		return nil, read, ERR_BAD_REQUEST
 	}
 
 	return &RequestLine{
-		Method:        parts[0],
-		RequestTarget: parts[1],
-		HttpVersion:   parts[2][len(parts[2])-3:],
-	}, restOfMsg, nil
+		Method:        string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HttpVersion:   string(parts[2][len(parts[2])-3:]),
+	}, read, nil
 
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := newRequest()
+
+	buf := make([]byte, 1024)
+	bufIdx := 0
+	for !request.done() {
+		n, err := reader.Read(buf[bufIdx:])
+		if err != nil {
+			return nil, err
+		}
+
+		readN, err := request.parse(buf[:bufIdx+n])
+		if err != nil {
+			return nil, err
+		}
+
+		bufIdx += n
+		copy(buf, buf[readN:bufIdx])
+		bufIdx -= readN
+
+	}
 	data, err := io.ReadAll(reader)
 	if err != nil {
 		return nil, err
